01630194fcbca1899639d777a900dd430fcce650
Anton Shepilov <anton.shepilov@gmail.com>
2023-04-13 15:27:10 +0200

    🐳 Set up envs for the TDrive (#19)
    - remove php upstream from nginx config
    - fix docker-compose configuration

--- tdrive/docker/tdrive-frontend/entrypoint.sh
+++ tdrive/docker/tdrive-frontend/entrypoint.sh
@@ -39,10 +39,8 @@ case $SSL_CERTS in
 esac
 
 NODE_HOST="${NODE_HOST:-http://node:3000}"
-PHP_UPSTREAM="${PHP_UPSTREAM:-php:9000}"
 export NODE_HOST
 export PHP_UPSTREAM
 envsubst '$${NODE_HOST} $${NGINX_LISTEN}' < /etc/nginx/sites-available/site.template > /etc/nginx/sites-enabled/site
-echo "upstream php-upstream { server ${PHP_UPSTREAM}; }" > /etc/nginx/conf.d/upstream.conf
 
 nginx -g "daemon off;"
