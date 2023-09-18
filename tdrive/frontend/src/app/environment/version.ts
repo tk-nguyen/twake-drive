43a5dbddf1058afac32a85a9cb7aac0321ca40df
Anton Shepilov <anton.shepilov@gmail.com>
2023-09-18 21:13:39 +0200

    🌟 Display TDrive version (#209)

--- tdrive/frontend/src/app/environment/version.ts
+++ tdrive/frontend/src/app/environment/version.ts
@@ -1,5 +1,5 @@
 export default {
-  version: /* @VERSION */ '2023.Q1',
-  version_detail: /* @VERSION_DETAIL */ '2023.Q1.1223',
-  version_name: /* @VERSION_NAME */ 'Albatros',
+  version: /* @VERSION */ '2023.Q3.011',
+  version_detail: /* @VERSION_DETAIL */ '2023.Q3.011',
+  version_name: /* @VERSION_NAME */ 'Ghost-Dog',
 };
--- tdrive/frontend/src/app/views/client/header/index.tsx
+++ tdrive/frontend/src/app/views/client/header/index.tsx
@@ -2,21 +2,30 @@ import { MenuIcon } from '@heroicons/react/outline';
 import Account from '../common/account';
 import AppGrid from '../common/app-grid';
 import Search from '../common/search';
+import { Info } from "@atoms/text";
+import version from '../../../environment/version';
 
 export default ({ openSideMenu }: { openSideMenu: () => void }) => {
   return (
     <div className="bg-white dark:bg-zinc-900 h-16 sm:h-20 p-4 sm:p-6 flex space-between items-center">
       <div className="sm:block hidden shrink-0 w-1/6 max-w-xs" style={{ minWidth: 100 }}>
-        <img
-          src="/public/img/logo/logo-text-black.png"
-          className="h-6 ml-1 dark:hidden block"
-          alt="Tdrive"
-        />
-        <img
-          src="/public/img/logo/logo-text-white.png"
-          className="h-6 ml-1 dark:block hidden"
-          alt="Tdrive"
-        />
+        <div className="sm:inline-grid">
+          <img
+            src="/public/img/logo/logo-text-black.png"
+            className="h-6 ml-1 dark:hidden block"
+            alt="Tdrive"
+          />
+          <img
+            src="/public/img/logo/logo-text-white.png"
+            className="h-6 ml-1 dark:block hidden"
+            alt="Tdrive"
+          />
+        </div>
+        <div className="sm:inline-grid">
+          <Info className="font-bold overflow-hidden text-ellipsis whitespace-nowrap w-full block -mb-1">
+            &nbsp;v{version.version}
+          </Info>
+        </div>
       </div>
       <div
         onClick={() => openSideMenu()}
