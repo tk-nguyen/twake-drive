065cd03f363375a65d9b5270fcf2d01bb6aa435a
Anton Shepilov <anton.shepilov@gmail.com>
2024-01-16 18:06:40 +0300

    ➖ Remove unoconv python library (#322)
    * ➖Remove unoconv python library

--- tdrive/backend/node/src/services/previews/services/files/processing/service.ts
+++ tdrive/backend/node/src/services/previews/services/files/processing/service.ts
@@ -1,5 +1,4 @@
 import { generatePreview as thumbnailsFromImages } from "./image";
-import { convertFromOffice } from "./office";
 import { convertFromPdf } from "./pdf";
 import { isFileType } from "../../../utils";
 import {
@@ -10,7 +9,11 @@ import {
 } from "../../../../../utils/mime";
 import { PreviewMessageQueueRequest, ThumbnailResult } from "../../../types";
 import { generateVideoPreview } from "./video";
-import { Initializable, TdriveServiceProvider } from "../../../../../core/platform/framework";
+import {
+  Initializable,
+  logger,
+  TdriveServiceProvider,
+} from "../../../../../core/platform/framework";
 import { cleanFiles } from "../../../../../utils/files";
 
 export class PreviewProcessService implements TdriveServiceProvider, Initializable {
@@ -26,17 +29,7 @@ export class PreviewProcessService implements TdriveServiceProvider, Initializab
     options: PreviewMessageQueueRequest["output"],
   ): Promise<ThumbnailResult[]> {
     if (isFileType(document.mime, document.filename, officeExtensions)) {
-      const pdfPath = await convertFromOffice(document.path, {
-        numberOfPages: options.pages,
-      });
-      await cleanFiles([document.path]);
-      const thumbnailPath = await convertFromPdf(pdfPath.output, {
-        numberOfPages: options.pages,
-      });
-      await cleanFiles([pdfPath.output]);
-      const images = (await thumbnailsFromImages(thumbnailPath.output, options)).output;
-      await cleanFiles(thumbnailPath.output);
-      return images;
+      logger.info("Preview generation for office files currently not supported");
     }
 
     if (isFileType(document.mime, document.filename, pdfExtensions)) {
