123e6212fbe2b765a98f398a3639bfaea057be98
Montassar Ghanmy <Monta.ghanmy@gmail.com>
2024-07-24 14:28:55 +0100

    🐛 Disable upload to trash/shared with me (#627)

--- tdrive/frontend/src/app/views/client/body/drive/browser.tsx
+++ tdrive/frontend/src/app/views/client/body/drive/browser.tsx
@@ -292,6 +292,7 @@ export default memo(
             }}
             onDragOver={handleDragOver}
             onDrop={handleDrop}
+            disabled={inTrash || access === 'read'}
           >
             {role == 'admin' && <UsersModal />}
             <VersionsModal />
